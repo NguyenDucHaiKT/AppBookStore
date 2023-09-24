40e6512113319704b71f7bfe91114017fc5f4069
NguyenDucHaiKT <nguyenduchaisystem@gmail.com>
2023-09-24 08:39:14 +0700

    [HaiND43][update file project]

---
+++ AppBookStore/Model/ModelBook.cpp
@@ -0,0 +1,6 @@
+#include "ModelBook.h"
+
+ModelBook::ModelBook()
+{
+
+}
---
+++ AppBookStore/Model/ModelBook.h
@@ -0,0 +1,13 @@
+#ifndef MODELBOOK_H
+#define MODELBOOK_H
+
+#include <QObject>
+
+class ModelBook
+{
+    Q_OBJECT
+public:
+    ModelBook();
+};
+
+#endif // MODELBOOK_H
